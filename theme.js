dfca1c3b6c4ac10bdffcb14ebc5b1fff83ec63a6
Guille De Fermin <guillermo.defermin@taxfix.de>
2020-06-16 15:48:49 +0200

    Initial commit

---
+++ theme.js
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    text: 'black',
+    background: 'white',
+    primary: '#32C850',
+  },
+  styles: {
+    li: {
+      paddingTop: 2,
+      paddingBottom: 2,
+    },
+    Slide: {
+      padding: 6,
+    },
+  },
+};
